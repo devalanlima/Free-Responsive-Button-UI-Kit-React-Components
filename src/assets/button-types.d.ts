e4f0107de8219224c9f6c9cb5c2fa035922f33c9
Alan Lima <dev.alanlima@gmail.com>
2024-09-25 15:19:36 -0300

    feat: Create button types

---
+++ src/assets/button-types.d.ts
@@ -0,0 +1,26 @@
+export type Corners = 
+| 'smooth'
+| 'edgy'
+
+export type Size = 
+| 'small'
+| 'medium'
+| 'large'
+
+export type State =
+| 'default'
+| 'click'
+| 'hover'
+| 'disabled'
+
+export type Style =
+| 'primary'
+| 'secondary'
+| 'outline'
+| 'ghost'
+
+export type Icon =
+| 'none'
+| 'right'
+| 'left'
+| 'only'
