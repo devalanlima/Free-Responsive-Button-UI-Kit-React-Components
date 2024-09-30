ccfb8b3fc031051e5a5fddfd4d749edca6542f24
Alan Lima <dev.alanlima@gmail.com>
2024-09-30 14:33:18 -0300

    feat: Add icons props to component

--- src/assets/components/Button.tsx
+++ src/assets/components/Button.tsx
@@ -1,75 +1,78 @@
 import { useState } from "react"
-import { Corners, Size, State, Style } from "../button-types"
+import { Corners, Icon, Size, State, Style } from "../button-types"
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode
   corners?: Corners
   size?: Size
   styleType?: Style
+  icon?: React.ReactNode
+  iconPosition?: Icon
 }
 
+interface ButtonWithIcon extends Props {
+  icon: React.ReactNode
+  iconPosition: Icon
+}
+
+interface ButtonWithoutIcon extends Props {
+  icon?: undefined
+  iconPosition?: undefined
+}
+
+type BtnProps = ButtonWithIcon | ButtonWithoutIcon
+
 export default function Button ({
-  children, 
-  corners = 'edgy', 
+  corners = 'smooth', 
   size = 'medium', 
   styleType = 'primary',
+  children, 
   disabled,
+  icon,
+  iconPosition,
   ...rest
-}: Props) {
+}: BtnProps) {
 
-  const btnStyle: Record<Style, string> = {
-    primary: 'bg-primary-medium text-gray-light',
-    secondary: 'bg-secondary-medium text-gray-light',
-    outline: 'bg-gray-light text-primary-medium border-[1px] border-primary-medium',
-    ghost: 'bg-none text-primary-medium',
+  const btnStyle: Record<Style, Record<State, string>> = {
+    primary: {
+      default: 'bg-primary-medium text-gray-light',
+      hover: 'hover:shadow-default-btn-hover hover:shadow-primary-dark/50 focus-visible:shadow-default-btn-hover focus-visible:shadow-primary-dark/50',
+      click: 'bg-primary-dark text-gray-light',
+      disabled: 'disabled:bg-primary-dark disabled:text-primary-medium',
+    },
+    secondary: {
+      default: 'bg-secondary-medium text-gray-light',
+      hover: 'hover:shadow-default-btn-hover hover:shadow-secondary-dark/50 focus-visible:shadow-default-btn-hover focus-visible:shadow-secondary-dark/50',
+      click: 'bg-secondary-dark text-gray-light',
+      disabled: 'disabled:bg-secondary-dark disabled:text-secondary-medium',
+    },
+    outline: {
+      default: 'bg-gray-light text-primary-medium border-[1px] border-primary-medium',
+      hover: 'hover:shadow-default-btn-hover hover:shadow-primary-dark/50 focus-visible:shadow-default-btn-hover focus-visible:shadow-primary-dark/50',
+      click: 'bg-primary-medium text-gray-light border-[1px] border-gray-light',
+      disabled: 'disabled:bg-gray-light disabled:text-[#CFCFCF] disabled:border-none',
+    },
+    ghost: {
+      default: 'bg-none text-primary-medium',
+      hover: 'hover:shadow-ghost-btn-hover hover:shadow-primary-dark/40 hover:text-primary-dark focus-visible:shadow-ghost-btn-hover focus-visible:shadow-primary-dark/40 focus-visible:text-primary-dark',
+      click: 'bg-primary-light text-primary-dark shadow-ghost-btn-hover shadow-primary-dark/40',
+      disabled: 'disabled:text-gray-medium',
+    },
   }
 
   const state: Record<State, string> = {
-    default: btnStyle[styleType],
-    hover: ((): string=>{
-      switch (styleType) {
-        case 'primary':
-          return `hover:shadow-default-btn-hover hover:shadow-primary-dark/50`
-        case 'secondary':
-          return `hover:shadow-default-btn-hover hover:shadow-secondary-dark/50`
-        case 'ghost':
-          return `hover:shadow-ghost-btn-hover hover:shadow-primary-dark/40 hover:text-primary-dark`
-        case 'outline':
-          return `hover:shadow-default-btn-hover hover:shadow-primary-dark/50`
-      }
-    })(),
-    click: (() :string => {
-      switch (styleType) {
-        case 'primary':
-          return 'bg-primary-dark text-gray-light'
-        case 'secondary':
-          return 'bg-secondary-dark text-gray-light'
-        case 'ghost':
-          return 'bg-primary-light text-primary-dark shadow-ghost-btn-hover shadow-primary-dark/40'
-        case 'outline':
-          return 'bg-primary-medium text-gray-light border-[1px] border-gray-light'
-      }
-    })(),
-    disabled: (() :string => {
-      switch (styleType) {
-        case 'primary':
-          return 'disabled:bg-primary-dark disabled:text-primary-medium'
-        case 'secondary':
-          return 'disabled:bg-secondary-dark disabled:text-secondary-medium'
-        case 'ghost':
-          return 'disabled:text-gray-medium'
-        case 'outline':
-          return 'disabled:bg-gray-light disabled:text-[#CFCFCF] disabled:border-none'
-      }
-    })(),
+    default: btnStyle[styleType].default,
+    hover: btnStyle[styleType].hover,
+    click: btnStyle[styleType].click,
+    disabled: btnStyle[styleType].disabled,
   }
 
   const [btnState, setBtnState] = useState(state.default)
 
   const btnSize: Record<Size, string> = {
-    small: 'py-[9px] px-[14px] text-[12px]',
-    medium: 'px-[18px] pt-[11px] pb-[12px] text-[14px]',
-    large: 'px-[24px] pt-[15px] pb-[16px] text-[16px]',
+    small: iconPosition === 'only' ? 'p-[9px] text-[12px]' : 'py-[9px] px-[14px] text-[12px]',
+    medium: iconPosition === 'only' ? 'p-[12px] text-[14px]' : 'px-[18px] pt-[11px] pb-[12px] text-[14px]',
+    large: iconPosition === 'only' ? 'p-[15px] text-[16px]' : 'px-[24px] pt-[15px] pb-[16px] text-[16px]',
   }
 
   const btnCorners: Record<Corners, string> = {
@@ -99,24 +102,62 @@ export default function Button ({
     }
   }
 
+  const enableLeftIcon = (): boolean => {
+    if (iconPosition !== 'none' && (((icon && iconPosition === 'left' || iconPosition === 'only')))) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const enableRightIcon = (): boolean => {
+    if ((iconPosition !== 'none') && (icon && iconPosition === 'right')) {
+      return true
+    } else { 
+      return false
+    }
+  }
+
+  const iconSize: Record<Size, string> = {
+    small: 'w-[14px]',
+    medium: 'w-[16px]',
+    large: 'w-[20px]',
+  }
+
   return (
     <button
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
       onKeyDown={handleKeyDown}
       onKeyUp={handleKeyUp}
-      disabled={disabled}    
+      onBlur={handleMouseUp}
+      disabled={disabled}          
       className={`
         ${btnState} 
         ${btnCorners[corners]} 
         ${btnSize[size]} 
         ${btnTyphograpy}
         ${state.disabled}
-        ${state.hover}
+        ${!disabled && state.hover}
+        flex gap-[8px]
       `}
       {...rest}
     >
-      {children}
+      {
+        enableLeftIcon() && 
+        <span className={`${iconPosition === 'only' && iconSize[size]}`}>
+          {icon}
+        </span>
+      }
+
+      {iconPosition !== 'only' && children}
+
+      {
+        enableRightIcon() && 
+        <span>
+          {icon}
+        </span>
+      }
     </button>
   )
 }
